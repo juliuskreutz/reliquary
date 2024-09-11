a2b064de21427eb549de6105b8569491bd23440d
Julius Kreutz <julius@kreutz.dev>
2024-09-11 15:33:15 +1000

    update: 2.5 protos

--- src/network/gen/proto/SyncLineupReason.rs
+++ src/network/gen/proto/SyncLineupReason.rs
@@ -19,69 +19,69 @@
 #![allow(unused_results)]
 #![allow(unused_mut)]
 
-//! Generated file from `SyncReason.proto`
+//! Generated file from `SyncLineupReason.proto`
 
 /// Generated files are compatible only with the same version
 /// of protobuf runtime.
 const _PROTOBUF_VERSION_CHECK: () = ::protobuf::VERSION_3_4_0;
 
 #[derive(Clone,Copy,PartialEq,Eq,Debug,Hash)]
-// @@protoc_insertion_point(enum:SyncReason)
-pub enum SyncReason {
-    // @@protoc_insertion_point(enum_value:SyncReason.SYNC_REASON_NONE)
+// @@protoc_insertion_point(enum:SyncLineupReason)
+pub enum SyncLineupReason {
+    // @@protoc_insertion_point(enum_value:SyncLineupReason.SYNC_REASON_NONE)
     SYNC_REASON_NONE = 0,
-    // @@protoc_insertion_point(enum_value:SyncReason.SYNC_REASON_MP_ADD)
+    // @@protoc_insertion_point(enum_value:SyncLineupReason.SYNC_REASON_MP_ADD)
     SYNC_REASON_MP_ADD = 1,
-    // @@protoc_insertion_point(enum_value:SyncReason.SYNC_REASON_MP_ADD_PROP_HIT)
+    // @@protoc_insertion_point(enum_value:SyncLineupReason.SYNC_REASON_MP_ADD_PROP_HIT)
     SYNC_REASON_MP_ADD_PROP_HIT = 2,
-    // @@protoc_insertion_point(enum_value:SyncReason.SYNC_REASON_HP_ADD)
+    // @@protoc_insertion_point(enum_value:SyncLineupReason.SYNC_REASON_HP_ADD)
     SYNC_REASON_HP_ADD = 3,
-    // @@protoc_insertion_point(enum_value:SyncReason.SYNC_REASON_HP_ADD_PROP_HIT)
+    // @@protoc_insertion_point(enum_value:SyncLineupReason.SYNC_REASON_HP_ADD_PROP_HIT)
     SYNC_REASON_HP_ADD_PROP_HIT = 4,
 }
 
-impl ::protobuf::Enum for SyncReason {
-    const NAME: &'static str = "SyncReason";
+impl ::protobuf::Enum for SyncLineupReason {
+    const NAME: &'static str = "SyncLineupReason";
 
     fn value(&self) -> i32 {
         *self as i32
     }
 
-    fn from_i32(value: i32) -> ::std::option::Option<SyncReason> {
+    fn from_i32(value: i32) -> ::std::option::Option<SyncLineupReason> {
         match value {
-            0 => ::std::option::Option::Some(SyncReason::SYNC_REASON_NONE),
-            1 => ::std::option::Option::Some(SyncReason::SYNC_REASON_MP_ADD),
-            2 => ::std::option::Option::Some(SyncReason::SYNC_REASON_MP_ADD_PROP_HIT),
-            3 => ::std::option::Option::Some(SyncReason::SYNC_REASON_HP_ADD),
-            4 => ::std::option::Option::Some(SyncReason::SYNC_REASON_HP_ADD_PROP_HIT),
+            0 => ::std::option::Option::Some(SyncLineupReason::SYNC_REASON_NONE),
+            1 => ::std::option::Option::Some(SyncLineupReason::SYNC_REASON_MP_ADD),
+            2 => ::std::option::Option::Some(SyncLineupReason::SYNC_REASON_MP_ADD_PROP_HIT),
+            3 => ::std::option::Option::Some(SyncLineupReason::SYNC_REASON_HP_ADD),
+            4 => ::std::option::Option::Some(SyncLineupReason::SYNC_REASON_HP_ADD_PROP_HIT),
             _ => ::std::option::Option::None
         }
     }
 
-    fn from_str(str: &str) -> ::std::option::Option<SyncReason> {
+    fn from_str(str: &str) -> ::std::option::Option<SyncLineupReason> {
         match str {
-            "SYNC_REASON_NONE" => ::std::option::Option::Some(SyncReason::SYNC_REASON_NONE),
-            "SYNC_REASON_MP_ADD" => ::std::option::Option::Some(SyncReason::SYNC_REASON_MP_ADD),
-            "SYNC_REASON_MP_ADD_PROP_HIT" => ::std::option::Option::Some(SyncReason::SYNC_REASON_MP_ADD_PROP_HIT),
-            "SYNC_REASON_HP_ADD" => ::std::option::Option::Some(SyncReason::SYNC_REASON_HP_ADD),
-            "SYNC_REASON_HP_ADD_PROP_HIT" => ::std::option::Option::Some(SyncReason::SYNC_REASON_HP_ADD_PROP_HIT),
+            "SYNC_REASON_NONE" => ::std::option::Option::Some(SyncLineupReason::SYNC_REASON_NONE),
+            "SYNC_REASON_MP_ADD" => ::std::option::Option::Some(SyncLineupReason::SYNC_REASON_MP_ADD),
+            "SYNC_REASON_MP_ADD_PROP_HIT" => ::std::option::Option::Some(SyncLineupReason::SYNC_REASON_MP_ADD_PROP_HIT),
+            "SYNC_REASON_HP_ADD" => ::std::option::Option::Some(SyncLineupReason::SYNC_REASON_HP_ADD),
+            "SYNC_REASON_HP_ADD_PROP_HIT" => ::std::option::Option::Some(SyncLineupReason::SYNC_REASON_HP_ADD_PROP_HIT),
             _ => ::std::option::Option::None
         }
     }
 
-    const VALUES: &'static [SyncReason] = &[
-        SyncReason::SYNC_REASON_NONE,
-        SyncReason::SYNC_REASON_MP_ADD,
-        SyncReason::SYNC_REASON_MP_ADD_PROP_HIT,
-        SyncReason::SYNC_REASON_HP_ADD,
-        SyncReason::SYNC_REASON_HP_ADD_PROP_HIT,
+    const VALUES: &'static [SyncLineupReason] = &[
+        SyncLineupReason::SYNC_REASON_NONE,
+        SyncLineupReason::SYNC_REASON_MP_ADD,
+        SyncLineupReason::SYNC_REASON_MP_ADD_PROP_HIT,
+        SyncLineupReason::SYNC_REASON_HP_ADD,
+        SyncLineupReason::SYNC_REASON_HP_ADD_PROP_HIT,
     ];
 }
 
-impl ::protobuf::EnumFull for SyncReason {
+impl ::protobuf::EnumFull for SyncLineupReason {
     fn enum_descriptor() -> ::protobuf::reflect::EnumDescriptor {
         static descriptor: ::protobuf::rt::Lazy<::protobuf::reflect::EnumDescriptor> = ::protobuf::rt::Lazy::new();
-        descriptor.get(|| file_descriptor().enum_by_package_relative_name("SyncReason").unwrap()).clone()
+        descriptor.get(|| file_descriptor().enum_by_package_relative_name("SyncLineupReason").unwrap()).clone()
     }
 
     fn descriptor(&self) -> ::protobuf::reflect::EnumValueDescriptor {
@@ -90,24 +90,24 @@ impl ::protobuf::EnumFull for SyncReason {
     }
 }
 
-impl ::std::default::Default for SyncReason {
+impl ::std::default::Default for SyncLineupReason {
     fn default() -> Self {
-        SyncReason::SYNC_REASON_NONE
+        SyncLineupReason::SYNC_REASON_NONE
     }
 }
 
-impl SyncReason {
+impl SyncLineupReason {
     fn generated_enum_descriptor_data() -> ::protobuf::reflect::GeneratedEnumDescriptorData {
-        ::protobuf::reflect::GeneratedEnumDescriptorData::new::<SyncReason>("SyncReason")
+        ::protobuf::reflect::GeneratedEnumDescriptorData::new::<SyncLineupReason>("SyncLineupReason")
     }
 }
 
 static file_descriptor_proto_data: &'static [u8] = b"\
-    \n\x10SyncReason.proto*\x94\x01\n\nSyncReason\x12\x14\n\x10SYNC_REASON_N\
-    ONE\x10\0\x12\x16\n\x12SYNC_REASON_MP_ADD\x10\x01\x12\x1f\n\x1bSYNC_REAS\
-    ON_MP_ADD_PROP_HIT\x10\x02\x12\x16\n\x12SYNC_REASON_HP_ADD\x10\x03\x12\
-    \x1f\n\x1bSYNC_REASON_HP_ADD_PROP_HIT\x10\x04B\x15\n\x13emu.lunarcore.pr\
-    otob\x06proto3\
+    \n\x16SyncLineupReason.proto*\x9a\x01\n\x10SyncLineupReason\x12\x14\n\
+    \x10SYNC_REASON_NONE\x10\0\x12\x16\n\x12SYNC_REASON_MP_ADD\x10\x01\x12\
+    \x1f\n\x1bSYNC_REASON_MP_ADD_PROP_HIT\x10\x02\x12\x16\n\x12SYNC_REASON_H\
+    P_ADD\x10\x03\x12\x1f\n\x1bSYNC_REASON_HP_ADD_PROP_HIT\x10\x04B\x15\n\
+    \x13emu.lunarcore.protob\x06proto3\
 ";
 
 /// `FileDescriptorProto` object which was a source for this generated file
@@ -127,7 +127,7 @@ pub fn file_descriptor() -> &'static ::protobuf::reflect::FileDescriptor {
             let mut deps = ::std::vec::Vec::with_capacity(0);
             let mut messages = ::std::vec::Vec::with_capacity(0);
             let mut enums = ::std::vec::Vec::with_capacity(1);
-            enums.push(SyncReason::generated_enum_descriptor_data());
+            enums.push(SyncLineupReason::generated_enum_descriptor_data());
             ::protobuf::reflect::GeneratedFileDescriptor::new_generated(
                 file_descriptor_proto(),
                 deps,
